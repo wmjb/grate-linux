ce85662a3d5917e5bf91d7b79a81212d5b6e0b32
Jonas Schwöbel <jonasschwoebel@yahoo.de>
2021-09-28 16:36:24 +0200

    ARM: tegra: surface-2: Add tCover support
    Patched I2C driver to accept address 0x00
    
    Signed-off-by: Jonas Schwöbel <jonasschwoebel@yahoo.de>

--- drivers/i2c/i2c-core-base.c
+++ drivers/i2c/i2c-core-base.c
@@ -824,7 +824,7 @@ static int i2c_check_addr_validity(unsigned int addr, unsigned short flags)
 {
 	// Fix for Surface RT tCover support.
 	// tCover uses address 0x00 which is reserved for general call
-	if (addr == 0x00 && of_machine_is_compatible("microsoft,surface-rt")) {
+	if (addr == 0x00 && (of_machine_is_compatible("microsoft,surface-rt") || of_machine_is_compatible("microsoft,surface-2"))) {
 		return 0;
 	}
 
@@ -858,7 +858,7 @@ int i2c_check_7bit_addr_validity_strict(unsigned short addr)
 	 */
 	// Fix for Surface RT tCover support.
 	// tCover uses address 0x00 which is reserved for general call
-	if (addr == 0x00 && of_machine_is_compatible("microsoft,surface-rt")) {
+	if (addr == 0x00 && (of_machine_is_compatible("microsoft,surface-rt") || of_machine_is_compatible("microsoft,surface-2"))) {
 		return 0;
 	}
 
